2c5490ad67b077f723c02b267cdeda79f9ebb0bd
PieDemon <gooddemon3@gmail.com>
2026-06-15 10:05:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10000)
 
 # Run the web server in a separate thread so it doesn't block the bot
 threading.Thread(target=run).start()
